abe3ea6d8a3a41bb3bab4b455ccc7c40168edb52
Duyet Nguyen <duyet.vn@gmail.com>
2018-09-29 07:32:47 +0000

    add model question and choice

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,23 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+import datetime
+from django.utils import timezone
 
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_next = models.CharField(max_length=255)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_next
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_next = models.CharField(max_length=255)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_next
